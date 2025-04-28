cc0599d6d6c798b2eb239ed8055cce377566a6be
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-04-28 12:07:21 -0400

    remove deprecated jest calls

--- tests/channel/socketio-channel.test.ts
+++ tests/channel/socketio-channel.test.ts
@@ -1,89 +1,93 @@
-import { SocketIoChannel } from '../../src/channel';
-import type { Socket } from 'socket.io-client';
-import { Connector } from '../../src/connector';
+import type { Socket } from "socket.io-client";
+import { SocketIoChannel } from "../../src/channel";
+import { Connector } from "../../src/connector";
 
-describe('SocketIoChannel', () => {
+describe("SocketIoChannel", () => {
     let channel: SocketIoChannel;
     let socket: Socket;
 
     beforeEach(() => {
-        const channelName = 'some.channel';
+        const channelName = "some.channel";
         let listeners: any[] = [];
         socket = {
             emit: (event: any, data: unknown) => {
-                listeners.filter(([e]) => e === event).forEach(([, fn]) => fn(channelName, data));
+                listeners
+                    .filter(([e]) => e === event)
+                    .forEach(([, fn]) => fn(channelName, data));
             },
             on: (event: any, fn): any => listeners.push([event, fn]),
             removeListener: (event: any, fn: any) => {
-                listeners = listeners.filter(([e, f]) => (!fn ? e !== event : e !== event || f !== fn));
+                listeners = listeners.filter(([e, f]) =>
+                    !fn ? e !== event : e !== event || f !== fn
+                );
             },
         } as Socket;
 
         channel = new SocketIoChannel(socket, channelName, {
-            broadcaster: 'socket.io',
+            broadcaster: "socket.io",
             ...Connector._defaultOptions,
             namespace: false,
         });
     });
 
-    test('triggers all listeners for an event', () => {
+    test("triggers all listeners for an event", () => {
         const l1 = jest.fn();
         const l2 = jest.fn();
         const l3 = jest.fn();
-        channel.listen('MyEvent', l1);
-        channel.listen('MyEvent', l2);
-        channel.listen('MyOtherEvent', l3);
+        channel.listen("MyEvent", l1);
+        channel.listen("MyEvent", l2);
+        channel.listen("MyOtherEvent", l3);
 
-        socket.emit('MyEvent', {});
+        socket.emit("MyEvent", {});
 
-        expect(l1).toBeCalled();
-        expect(l2).toBeCalled();
-        expect(l3).not.toBeCalled();
+        expect(l1).toHaveBeenCalled();
+        expect(l2).toHaveBeenCalled();
+        expect(l3).not.toHaveBeenCalled();
 
-        socket.emit('MyOtherEvent', {});
+        socket.emit("MyOtherEvent", {});
 
-        expect(l3).toBeCalled();
+        expect(l3).toHaveBeenCalled();
     });
 
-    test('can remove a listener for an event', () => {
+    test("can remove a listener for an event", () => {
         const l1 = jest.fn();
         const l2 = jest.fn();
         const l3 = jest.fn();
-        channel.listen('MyEvent', l1);
-        channel.listen('MyEvent', l2);
-        channel.listen('MyOtherEvent', l3);
+        channel.listen("MyEvent", l1);
+        channel.listen("MyEvent", l2);
+        channel.listen("MyOtherEvent", l3);
 
-        channel.stopListening('MyEvent', l1);
+        channel.stopListening("MyEvent", l1);
 
-        socket.emit('MyEvent', {});
+        socket.emit("MyEvent", {});
 
-        expect(l1).not.toBeCalled();
-        expect(l2).toBeCalled();
-        expect(l3).not.toBeCalled();
+        expect(l1).not.toHaveBeenCalled();
+        expect(l2).toHaveBeenCalled();
+        expect(l3).not.toHaveBeenCalled();
 
-        socket.emit('MyOtherEvent', {});
+        socket.emit("MyOtherEvent", {});
 
-        expect(l3).toBeCalled();
+        expect(l3).toHaveBeenCalled();
     });
 
-    test('can remove all listeners for an event', () => {
+    test("can remove all listeners for an event", () => {
         const l1 = jest.fn();
         const l2 = jest.fn();
         const l3 = jest.fn();
-        channel.listen('MyEvent', l1);
-        channel.listen('MyEvent', l2);
-        channel.listen('MyOtherEvent', l3);
+        channel.listen("MyEvent", l1);
+        channel.listen("MyEvent", l2);
+        channel.listen("MyOtherEvent", l3);
 
-        channel.stopListening('MyEvent');
+        channel.stopListening("MyEvent");
 
-        socket.emit('MyEvent', {});
+        socket.emit("MyEvent", {});
 
-        expect(l1).not.toBeCalled();
-        expect(l2).not.toBeCalled();
-        expect(l3).not.toBeCalled();
+        expect(l1).not.toHaveBeenCalled();
+        expect(l2).not.toHaveBeenCalled();
+        expect(l3).not.toHaveBeenCalled();
 
-        socket.emit('MyOtherEvent', {});
+        socket.emit("MyOtherEvent", {});
 
-        expect(l3).toBeCalled();
+        expect(l3).toHaveBeenCalled();
     });
 });
--- tests/echo.test.ts
+++ tests/echo.test.ts
@@ -1,32 +1,38 @@
-import Echo from '../src/echo';
-import { NullConnector } from '../src/connector';
+import { NullConnector } from "../src/connector";
+import Echo from "../src/echo";
 
-describe('Echo', () => {
-    test('it will not throw error for supported driver', () => {
-        expect(() => new Echo({ broadcaster: 'reverb' })).not.toThrowError(
-            'Broadcaster string reverb is not supported.'
+describe("Echo", () => {
+    test("it will not throw error for supported driver", () => {
+        expect(() => new Echo({ broadcaster: "reverb" })).not.toThrow(
+            "Broadcaster string reverb is not supported."
         );
 
-        expect(() => new Echo({ broadcaster: 'pusher' })).not.toThrowError(
-            'Broadcaster string pusher is not supported.'
+        expect(() => new Echo({ broadcaster: "pusher" })).not.toThrow(
+            "Broadcaster string pusher is not supported."
         );
 
-        expect(() => new Echo({ broadcaster: 'socket.io' })).not.toThrowError(
-            'Broadcaster string socket.io is not supported.'
+        expect(() => new Echo({ broadcaster: "socket.io" })).not.toThrow(
+            "Broadcaster string socket.io is not supported."
         );
 
-        expect(() => new Echo({ broadcaster: 'null' })).not.toThrowError('Broadcaster string null is not supported.');
-        expect(() => new Echo({ broadcaster: NullConnector })).not.toThrowError();
+        expect(() => new Echo({ broadcaster: "null" })).not.toThrow(
+            "Broadcaster string null is not supported."
+        );
+        expect(() => new Echo({ broadcaster: NullConnector })).not.toThrow();
 
         // eslint-disable-next-line
         // @ts-ignore
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        expect(() => new Echo({ broadcaster: () => {} })).not.toThrowError('Broadcaster function is not supported.');
+        expect(() => new Echo({ broadcaster: () => {} })).not.toThrow(
+            "Broadcaster function is not supported."
+        );
     });
 
-    test('it will throw error for unsupported driver', () => {
+    test("it will throw error for unsupported driver", () => {
         // eslint-disable-next-line
         // @ts-ignore
-        expect(() => new Echo({ broadcaster: 'foo' })).toThrowError('Broadcaster string foo is not supported.');
+        expect(() => new Echo({ broadcaster: "foo" })).toThrow(
+            "Broadcaster string foo is not supported."
+        );
     });
 });
