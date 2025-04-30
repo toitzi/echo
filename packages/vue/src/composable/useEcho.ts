cb1a25944031d3fcb485358a7d4e7b23ebb69e79
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-04-30 13:10:29 -0400

    vue config defaults

--- packages/vue/src/composable/useEcho.ts
+++ packages/vue/src/composable/useEcho.ts
@@ -21,6 +21,11 @@ type Channel = {
     private: boolean;
 };
 
+type ConfigDefaults<O extends BroadcastDriver> = Record<
+    O,
+    Broadcaster[O]["options"]
+>;
+
 let echoInstance: Echo<BroadcastDriver> | null = null;
 let echoConfig: EchoOptions<BroadcastDriver> | null = null;
 const channels: Record<string, ChannelData<BroadcastDriver>> = {};
@@ -98,10 +103,48 @@ const leaveChannel = (channel: Channel, leaveAll: boolean = false): void => {
     }
 };
 
+/**
+ * Configure the Echo instance with sensible defaults.
+ *
+ * @link https://laravel.com/docs/broadcasting#client-side-installation
+ */
 export const configureEcho = <T extends BroadcastDriver>(
     config: EchoOptions<T>,
 ): void => {
-    echoConfig = config;
+    const defaults: ConfigDefaults<BroadcastDriver> = {
+        reverb: {
+            broadcaster: "reverb",
+            key: import.meta.env.VITE_REVERB_KEY,
+            wsHost: import.meta.env.VITE_REVERB_HOST,
+            wsPort: import.meta.env.VITE_REVERB_PORT,
+            wssPort: import.meta.env.VITE_REVERB_PORT,
+            forceTLS:
+                (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+            enabledTransports: ["ws", "wss"],
+        },
+        pusher: {
+            broadcaster: "reverb",
+            key: import.meta.env.VITE_PUSHER_APP_KEY,
+            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+            forceTLS: true,
+            wsHost: import.meta.env.VITE_PUSHER_HOST,
+            wsPort: import.meta.env.VITE_PUSHER_PORT,
+            wssPort: import.meta.env.VITE_PUSHER_PORT,
+            enabledTransports: ["ws", "wss"],
+        },
+        "socket.io": {
+            broadcaster: "socket.io",
+            host: import.meta.env.VITE_SOCKET_IO_HOST,
+        },
+        null: {
+            broadcaster: "null",
+        },
+    };
+
+    echoConfig = {
+        ...defaults[config.broadcaster],
+        ...config,
+    } as EchoOptions<BroadcastDriver>;
 
     // Reset the instance if it was already created
     if (echoInstance) {
