2668d1bb9180abfaa394c47cce5fa7d8293213ea
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-04-30 13:04:02 -0400

    react defaults

--- packages/react/src/hook/use-echo.ts
+++ packages/react/src/hook/use-echo.ts
@@ -21,6 +21,11 @@ type Channel = {
     private: boolean;
 };
 
+type ConfigDefaults<O extends BroadcastDriver> = Record<
+    O,
+    Broadcaster[O]["options"]
+>;
+
 let echoInstance: Echo<BroadcastDriver> | null = null;
 let echoConfig: EchoOptions<BroadcastDriver> | null = null;
 const channels: Record<string, ChannelData<BroadcastDriver>> = {};
@@ -58,7 +63,40 @@ const leaveChannel = (channel: Channel, leaveAll: boolean): void => {
 export const configureEcho = <T extends BroadcastDriver>(
     config: EchoOptions<T>,
 ): void => {
-    echoConfig = config;
+    const defaults: ConfigDefaults<BroadcastDriver> = {
+        reverb: {
+            broadcaster: "reverb",
+            key: import.meta.env.VITE_REVERB_KEY,
+            wsHost: import.meta.env.VITE_REVERB_HOST,
+            wsPort: import.meta.env.VITE_REVERB_PORT,
+            wssPort: import.meta.env.VITE_REVERB_PORT,
+            forceTLS:
+                (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+            enabledTransports: ["ws", "wss"],
+        },
+        pusher: {
+            broadcaster: "reverb",
+            key: import.meta.env.VITE_PUSHER_APP_KEY,
+            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+            forceTLS: true,
+            wsHost: import.meta.env.VITE_PUSHER_HOST,
+            wsPort: import.meta.env.VITE_PUSHER_PORT,
+            wssPort: import.meta.env.VITE_PUSHER_PORT,
+            enabledTransports: ["ws", "wss"],
+        },
+        "socket.io": {
+            broadcaster: "socket.io",
+            host: import.meta.env.VITE_SOCKET_IO_HOST,
+        },
+        null: {
+            broadcaster: "null",
+        },
+    };
+
+    echoConfig = {
+        ...defaults[config.broadcaster],
+        ...config,
+    };
 
     // Reset the instance if it was already created
     if (echoInstance) {
---
+++ packages/react/src/vite-env.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUSHER_APP_CLUSTER: string;
+    readonly VITE_PUSHER_APP_KEY: string;
+    readonly VITE_PUSHER_HOST: string;
+    readonly VITE_PUSHER_PORT: number;
+
+    readonly VITE_REVERB_HOST: string;
+    readonly VITE_REVERB_KEY: string;
+    readonly VITE_REVERB_PORT: number;
+    readonly VITE_REVERB_SCHEME: string;
+
+    readonly VITE_SOCKET_IO_HOST: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
